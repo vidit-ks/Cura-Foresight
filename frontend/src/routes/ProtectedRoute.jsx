746c097e3d7827a881f5017fa47aa312f84c52cd
Vidit Kumar Singh <145222589+vidit-ks@users.noreply.github.com>
2026-03-03 21:07:29 +0530

    Update ProtectedRoute.jsx

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react"
 import { Navigate } from "react-router-dom"
-import { AuthContext } from "../context/authContext"
+import { AuthContext } from "../context/AuthContext"
 const ProtectedRoute = ({children, allowedRoles}) => {
   const {state} = useContext(AuthContext);
   const {role,token} = state;
